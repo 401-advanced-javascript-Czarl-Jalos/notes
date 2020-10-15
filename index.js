20e69fae7a76c125203a9bb929be9a83dc698dfe
Czarl Jalos <czarlfrancis@gmail.com>
2020-10-14 23:28:58 -0700

    added mongo connect

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
 'use strict';
-
+const mongoose = require('mongoose');
 const Input = require('./lib/input.js');
 const Notes = require('./lib/notes.js');
 
 const input = new Input(process.argv);
+
+if (input.action) {
 let obj =  new Notes(input);
-console.log(obj);
+obj.execute(input.action);
+} else {
+  throw new Error('invalid action');
+}
+mongoose.connect('mongodb://localhost:27017/Note', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+  .then(() => console.log('we are connected to mongo'));
